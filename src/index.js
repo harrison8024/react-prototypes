3921e16908a16a846c1ec5367d24877d004e5f91
harrison8024 <harrison8024@gmail.com>
2018-04-24 19:32:42 -0700

    react-intro Harrison Chen

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    return Math.floor(Math.random()*1001);
+}
+
+function greeting(user){
+    return <div><h1>Welcome {user.name}</h1><h2 className={"text-muted"} >Lucky Number is {user.luckyNumber}</h2></div>
+}
+const user = {
+    name: "Harrison",
+    luckyNumber: luckyNumber()
+};
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );

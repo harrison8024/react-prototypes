afd3b2d04465edf30d50c218eae9614f695ed6d2
harrison8024 <harrison8024@gmail.com>
2018-04-30 15:05:35 -0700

    created table.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,5 @@
+import React from 'react';
 
-
+export default ()=>{
+    return <h1>My first React functional component</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function luckyNumber(){
-    return Math.floor(Math.random()*1001);
-}
-
-function greeting(user){
-    return <div><h1>Welcome {user.name}</h1><h2 className={"text-muted"} >Lucky Number is {user.luckyNumber}</h2></div>
-}
-const user = {
-    name: "Harrison",
-    luckyNumber: luckyNumber()
-};
-
+import App from './components/app';
 
 ReactDOM.render(
-    greeting(user),
+    <App/>,
     document.getElementById('root')
 );
